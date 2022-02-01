d9ed0f1206cce4944bb4aad1df9482f4891802de
Aleks <aleksisafk@gmail.com>
2022-02-01 20:11:07 +0300

    connecting components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
+
+import {CartPage, HomePage} from "../pages";
 
 import './app.css';
 
 const App = () => {
     return (
         <div className='app'>
+            <Switch>
+                <Route path='/' exact component={HomePage}/>
 
+                <Route path='/cart' exact component={CartPage}/>
+
+                <Route render={() => <h2>This page does not exist</h2>}/>
+            </Switch>
         </div>
     );
 };
---
+++ src/components/pages/cart-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () => {
+    return (
+        <h2 className='cart-page'>
+            Welcome to Cart page
+        </h2>
+    );
+};
+
+export default CartPage;
